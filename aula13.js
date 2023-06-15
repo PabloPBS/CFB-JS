5e76c045ec71d305e8a65cf9ec11d28ca0456bb6
Pablo Barbosa Silva <126425360+PabloPBS@users.noreply.github.com>
2023-06-15 16:00:43 -0300

    aula 13 - 14

---
+++ aula13.js
@@ -0,0 +1,13 @@
+let num = 100;
+
+if (num > 10) {
+    console.log("Numeral maior que 10")
+    if (num > 50) {
+        console.log("Numeral maior que 50")
+    }
+} else if (num == 10) {
+    console.log("Numeral igual a 10")
+} else {
+    console.log("Numeral menor que 10")
+}
+console.log ("Fim do programa")
---
+++ aula14.js
@@ -0,0 +1,16 @@
+let colocacao = 4;
+
+switch (colocacao) {
+    case 1:
+        console.log ("Primeiro Lugar.")
+        break
+    case 2:
+        console.log ("Segindo Lugar.")
+        break
+    case 3:
+        console.log("Terceiro Lugar.")
+        break
+    default:
+        console.log("Não ficou no pódio.")
+        break
+}
