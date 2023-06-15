eb69cf971d7584dd12be421e0cf9d332483e23cc
Pablo Barbosa Silva <126425360+PabloPBS@users.noreply.github.com>
2023-06-15 16:38:03 -0300

    aula16

---
+++ aula16.js
@@ -0,0 +1,12 @@
+const objts = document.getElementsByTagName("div")
+let num = [10, 20, 30, 40, 50]
+
+//of - retorna os elementos
+for (o of objts) {
+    console.log(o)
+}
+
+//in - retorna as posições dos elementos
+for (o in objts) {
+     console.log(o)
+}
