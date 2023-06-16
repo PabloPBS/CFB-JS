968e82a4362a9e9b019064c15931d2336b59b94f
Pablo Barbosa Silva <126425360+PabloPBS@users.noreply.github.com>
2023-06-16 12:13:24 -0300

    aula20

---
+++ aula20.js
@@ -0,0 +1,8 @@
+function mudarTexto() {
+    d1 = document.getElementById("1")
+    d2 = document.getElementById("2")
+    d3 = document.getElementById("3")
+    d1.innerHTML = "CFB Cursos"
+    d2.innerHTML = "CFB Cursos"
+    d3.innerHTML = "CFB Cursos"
+}
