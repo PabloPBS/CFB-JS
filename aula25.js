a8294ecb041c54f8f68193578148f3ac281cbc5f
Pablo Barbosa Silva <126425360+PabloPBS@users.noreply.github.com>
2023-06-24 16:24:49 -0300

    aula25

---
+++ aula25.js
@@ -0,0 +1,14 @@
+const soma = (v1, v2) => {
+    let res = v1 + v2
+    return res
+}
+
+const nome = n => {return "Olá " + n}
+
+const add = n => n+10
+
+console.log(soma(5, 10))
+
+console.log(add(10))
+
+console.log(nome('Pablo'))
